dcd55061267b283510256af5f750689b659665a7
Daniel Tamakloe <37350796+301202@users.noreply.github.com>
2024-05-10 17:18:42 +0000

    The translator package messes up the online chat. will work on it next

--- clientSide.py
+++ clientSide.py
@@ -1,30 +1,32 @@
 import socket
 import threading
-import googletrans
-from googletrans import Translator
+#import googletrans
+#from googletrans import Translator
 
-translator = Translator()
+#translator = Translator()
 
 nickname = input("Choose a nickname: ")
 
-print("Here are the list of languages:")
-print(googletrans.LANGUAGES)
+#print("Here are the list of languages:")
+#print(googletrans.LANGUAGES)
 
-lang = input("Please enter your language of choice: ")
+#lang = input("Please enter your language of choice: ")
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('127.0.0.1', 55555))
+client.connect(("7.tcp.eu.ngrok.io", 14248))
+
 
 def receive():
     while True:
         try:
-            message =  client.recv(1024).decode()
-            detectedLang = translator.detect(message).lang.lower()
+            message =  client.recv(1024).decode('ascii')
+            #detectedLang = translator.detect(message).lang.lower()
             if message == 'NICK':
                 client.send(nickname.encode('ascii'))
             else:
-                translation = translator.translate(message, src=detectedLang, dest=lang)
-                print(translation.text)
+ #               translation = translator.translate(message, src=detectedLang, dest=lang)
+  #              print(translation.text)
+                print(message)
         except:
             print("An error occurred!")
             client.closed()
@@ -37,7 +39,7 @@ def write():
             client.close()
             break
         else:
-            client.send(f"{message}".encode('ascii'))
+            client.send(f"{message}".encode())
 
 receive_thread = threading.Thread(target=receive)
 receive_thread.start()
