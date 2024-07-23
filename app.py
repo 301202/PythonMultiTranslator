6b3ad0e27a35c852efcecafaad43daecc94caf5f
Daniel Tamakloe <37350796+301202@users.noreply.github.com>
2024-07-23 17:27:19 +0000

    more test

--- app.py
+++ app.py
@@ -122,9 +122,5 @@ def disconnect():
     print(f"{name} left the room {room}")
 
 if __name__ == "__main__":
-    # For local development
-    socketio.run(app, host="0.0.0.0", port=5000)
-else:
-    # For Vercel deployment
     port = int(os.environ.get('PORT', 5000))
     socketio.run(app, host='0.0.0.0', port=port)
