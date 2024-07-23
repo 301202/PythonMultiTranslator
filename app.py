5fed4e588fed599d972e759fa41e6ffd54563ea1
Daniel Tamakloe <37350796+301202@users.noreply.github.com>
2024-07-23 18:47:24 +0000

    port specified

--- app.py
+++ app.py
@@ -143,9 +143,5 @@ def leave():
 
 
 if __name__ == "__main__":
-    #Local development
-    socketio.run(app, debug=True)
-else:
-    #Production
     port = int(os.environ.get('PORT', 5000))
     socketio.run(app, host='0.0.0.0', port=port)
